08795bf576f0fe348768fbf3058ffc0f67bbe06a
Alex19901509 <147518422+Alex19901509@users.noreply.github.com>
2024-10-31 14:17:20 +0300

    dz++

--- config/settings.py
+++ config/settings.py
@@ -76,9 +76,9 @@ SIMPLE_JWT = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("NAME"),
+        "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("PASSWORD"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("HOST"),
         "PORT": os.getenv("PORT"),
     }
